553962eaa361de60644eda971bd41a33e0726d6e
whatevertogo <149563971+whatevertogo@users.noreply.github.com>
2025-02-25 00:45:36 +0800

    add initial project files and configuration for Unity project setup

---
+++ Assets/Managers/GameManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        // 单例
+        private static GameManager _instance;
+
+        public static GameManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindFirstObjectByType<GameManager>();
+                    if (_instance == null)
+                    {
+                        GameObject go = new GameObject(nameof(GameManager));
+                        _instance = go.AddComponent<GameManager>();
+                        DontDestroyOnLoad(go); // 保证 GameManager 在场景切换时不销毁
+                    }
+                }
+
+                return _instance;
+                
+            }
+            
+        }
+        
+    }
+    
+}

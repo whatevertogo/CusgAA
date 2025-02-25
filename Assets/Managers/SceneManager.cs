3d0a054cb9f0de1687bf00ac590b5d6355fba2f1
whatevertogo <149563971+whatevertogo@users.noreply.github.com>
2025-02-26 00:59:58 +0800

    修复杂鱼代码

--- Assets/Managers/SceneManager.cs
+++ Assets/Managers/SceneManager.cs
@@ -4,42 +4,8 @@ using UnityEngine.SceneManagement; // 引入场景管理命名空间
 
 namespace Managers
 {
-    public class MySceneManager : MonoBehaviour
+    public class MySceneManager :Singleton<MySceneManager>
     {
-        private static MySceneManager _instance;
-
-        public static MySceneManager Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = FindFirstObjectByType<MySceneManager>();
-                    if (_instance == null)
-                    {
-                        GameObject go = new GameObject(nameof(MySceneManager));
-                        _instance = go.AddComponent<MySceneManager>();
-                        DontDestroyOnLoad(go);
-                    }
-                }
-
-                return _instance;
-            }
-        }
-
-        private void Awake()
-        {
-            if (_instance == null)
-            {
-                _instance = this;
-                DontDestroyOnLoad(gameObject);
-            }
-            else if (_instance != this)
-            {
-                Destroy(gameObject);
-            }
-        }
-
         // Load scene by name
         public void LoadSceneByName(string sceneName)
         {

eb657a5aa3a09e0999d1fa858b68b81927afa791
whatevertogo <1879483647@qq.com>
2025-02-27 00:59:53 +0800

    1

--- Assets/Scripts/stopMenu/StopMenuPanel.cs
+++ Assets/Scripts/stopMenu/StopMenuPanel.cs
@@ -1,16 +1,29 @@
+using System;
 using UnityEngine;
 
 public class StopMenuPanel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool _isOpen;
+    [SerializeField] private GameObject panel;
+
+    private void Awake()
     {
-        
+        _isOpen = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MenuPanel();
+    }
+
+    private void MenuPanel()
+    {
+        // 检查是否按下了 ESC 键
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _isOpen = !_isOpen;
+            // 切换面板的激活状态
+            panel.SetActive(_isOpen);
+        }
     }
 }

1f57555036bc60a5e760d741da2271d0f05e98f9
whatevertogo <1879483647@qq.com>
2025-02-27 19:00:05 +0800

    添加VSCode配置文件，更新场景管理器，删除冗余文件，优化.gitignore

--- Assets/Managers/SceneManager.cs
+++ Assets/Managers/SceneManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement; // 引入场景管理命名空间
 
 namespace Managers
 {
-    public class MySceneManager :Singleton<MySceneManager>
+    public class MySceneManager : Singleton<MySceneManager>
     {
         // Load scene by name
         public void LoadSceneByName(string sceneName)
@@ -18,13 +18,13 @@ namespace Managers
         {
             SceneManager.LoadScene(sceneIndex);
         }
-        
+
         public void LoadSceneAsync(string sceneName)
         {
             StartCoroutine(LoadSceneAsyncCoroutine(sceneName));
         }
 
-        private IEnumerator LoadSceneAsyncCoroutine(string sceneName)
+        public IEnumerator LoadSceneAsyncCoroutine(string sceneName)
         {
             AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
             while (asyncOperation is not { isDone: true })
@@ -33,8 +33,17 @@ namespace Managers
                 yield return null;
             }
         }
-        
-        
-        
+
+        public void ReloadCurrentLevel()
+        {
+            // 获取当前活动场景
+            Scene currentScene = SceneManager.GetActiveScene();
+
+            // 重新加载当前场景
+            SceneManager.LoadScene(currentScene.name);
+        }
+
+
+
     }
 }

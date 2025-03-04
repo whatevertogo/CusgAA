860816e58c25bc28ce63fc846968b44275c9c946
whatevertogo <1879483647@qq.com>
2025-03-04 20:39:38 +0800

    可复用代码

--- Assets/Scripts/对话/DialogueController.cs
+++ Assets/Scripts/对话/DialogueController.cs
@@ -4,9 +4,10 @@ public class DialogueController : MonoBehaviour
 {
     [SerializeField] private DialogueControl dialogueControl;
 
-    private void Start()
+    protected virtual void Start()
     {
-        if (dialogueControl == null)
+
+        if (dialogueControl is null)
         {
             dialogueControl=GetComponent<DialogueControl>();
             Debug.Log("没有找到所属的dialogueControl尝试使用是否自带的component");

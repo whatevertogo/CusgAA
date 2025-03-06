3b76d1498610bf80dec21a1c1349f6d7420db4ae
吕东成 <1879483647@qq.com>
2025-03-06 10:43:32 +0800

    更新 IInteractable.cs

--- Assets/Scripts/互动/IInteractable.cs
+++ Assets/Scripts/互动/IInteractable.cs
@@ -2,7 +2,19 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class InterfaceReference<T>where T : class{
-    [SerializeField] private MonoBehaviour reference;
-    public T Value => reference as T;
+public class InterfaceReference<T> where T : class
+{
+    [SerializeField] private GameObject reference;  // 直接存 GameObject
+
+    public T Value
+    {
+        get
+        {
+            if (reference == null) return null;
+            T component = reference.GetComponent<T>();  // 获取第一个 T 类型的组件
+            if (component == null)
+                Debug.LogWarning($"[InterfaceReference] {reference.name} 没有实现 {typeof(T).Name}，请检查赋值！");
+            return component;
+        }
+    }
 }

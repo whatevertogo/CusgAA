e4540e09d9108f7b206a791ea256374b4b59164d
whatevertogo <1879483647@qq.com>
2025-03-10 17:28:06 +0800

    更新 CustomCursorChanger 以改善光标提示，添加自定义光标纹理说明，优化鼠标进入和离开事件的注释

--- Assets/Scripts/script-涂色游戏/CustomCursorChanger.cs
+++ Assets/Scripts/script-涂色游戏/CustomCursorChanger.cs
@@ -3,18 +3,18 @@ using UnityEngine.EventSystems;
 
 public class CustomCursorChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private Texture2D cursorTexture; // 在Inspector中指定每个方块对应的光标纹理
+    [SerializeField] private Texture2D cursorTexture; // 鍦↖nspector涓寚瀹氭瘡涓柟鍧楀搴旂殑鍏夋爣绾圭悊
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // 当鼠标进入方块时，设置自定义光标
+        // 褰撻紶鏍囪繘鍏ユ柟鍧楁椂锛岃缃嚜瀹氫箟鍏夋爣
         Vector2 hotspot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2);
         Cursor.SetCursor(cursorTexture, hotspot, CursorMode.Auto);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // 当鼠标离开方块时，恢复默认光标
+        // 褰撻紶鏍囩寮�鏂瑰潡鏃讹紝鎭㈠榛樿鍏夋爣
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 }

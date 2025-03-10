c63a1ae03fd679bbe06f7794aec13fd5ea6b89e2
王宇杰 <15378723+wang-yujiee@user.noreply.gitee.com>
2025-03-10 23:34:02 +0800

    添加管线空材质以及箱子交互加了点

--- Assets/Scripts/Allin/PasswordChest.cs
+++ Assets/Scripts/Allin/PasswordChest.cs
@@ -1,19 +1,42 @@
+using System;
 using UnityEngine;
+using  UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public class PasswordChest : TriggerObject
 {
+    [SerializeField] private GameObject passwordChestUI; 
+    [SerializeField] private Button _closeButton;
 
+
+    private void Start()
+    {
+        _closeButton.onClick.AddListener(ChestSet);
+    }
 
     public override void Interact(){
         //TODO-互动逻辑
-        //TODO-打开密码箱Panel
+        ChestSet();//设置箱子解锁面板显示
         //TODO-播放密码箱动画
         //TODO-播放密码箱音效
         Debug.Log("打开密码箱");
 
     }
+
+    private void ChestSet()
+    {
+        if (!passwordChestUI.activeSelf)
+        {
+            passwordChestUI.SetActive(true);
+        }
+        else
+        {
+            passwordChestUI.SetActive(false);
+        }
+    }
+    
 
     
 

8a87eb6d13ea45d680780f22eb9f7bdac24e1726
whatevertogo <1879483647@qq.com>
2025-03-14 14:03:39 +0800

    为音频管理器添加音量控制UI，更新音量设置逻辑以响应滑块变化

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Managers;
+using UnityEngine.UI;
 
 public class AudioManager : Singleton<AudioManager>
 {
@@ -17,8 +18,13 @@ public class AudioManager : Singleton<AudioManager>
     [SerializeField] private AudioSource bgmSource;
     [Tooltip("音效播放器")]
     [SerializeField] private AudioSource sfxSourcePrefab;
+    [Header("音效播放器列表")]
     private List<AudioSource> sfxSources;
     private Dictionary<string, AudioClip> AudioClipDictionary = new();
+
+    [Header("绑定UI条")]
+    [SerializeField ] private Slider masterVolumeSlider;
+    [SerializeField ] private Slider bgmVolumeSlider;
 
     // 音量改变事件，用于通知UI更新
     public System.Action onVolumeChanged;
@@ -47,12 +53,23 @@ public class AudioManager : Singleton<AudioManager>
     //     ChangeBGMClip(bgmSource, audioClipDataList[0].clip);
     // }//例子
 
-
-
     private void Start()
     {
         // 初始化音频源
         InitializeAudioSources();
+    }
+
+    private void Update(){
+        // 更新音量
+        if (masterVolumeSlider != null)
+        {
+            MasterVolume = masterVolumeSlider.value;
+        }
+
+        if (bgmVolumeSlider != null)
+        {
+            BGMVolume = bgmVolumeSlider.value;
+        }
     }
 
     private void InitializeAudioSources()
@@ -66,8 +83,6 @@ public class AudioManager : Singleton<AudioManager>
         }
 
     }
-
-
 
     /// <summary>
     /// 主音量
@@ -196,10 +211,5 @@ public class AudioManager : Singleton<AudioManager>
             Debug.Log("字典中没有clip值考虑是否没有在编辑器添加,或者并没有输入名字和audioSource，或者名字错误");
         }
     }
-
-
-
-
-
 
 }
